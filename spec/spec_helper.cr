866ec8d97a883e49583d483bb9015f5ec7c17a87
PenguinOwl <henryjboles@gmail.com>
2019-12-16 09:10:24 -0800

    init

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/precommit-test"
---
+++ src/precommit-test.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Precommit::Test`
+module Precommit::Test
+  VERSION = "0.1.0"
+
+  puts "CRYSTAL WORKS"
+end
